78e4d12c234e432aea021821e52d4c573f3a34a5
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-31 16:50:48 +0300

    adding function to calculate available hours

--- Education.py
+++ Education.py
@@ -13,3 +13,14 @@ df = pd.read_csv(r'C:/Users/Admin/Desktop/Education_Cleaned.csv')
 
 label_encoder = LabelEncoder()
 df['Level_encoded'] = label_encoder.fit_transform(df['Level'])
+
+def calculate_available_hours(start_date, end_date, daily_hours, weekly_days):
+    num_days = (end_date - start_date).days
+    if num_days < 0:
+        raise ValueError("Target date must be after the current date")
+    
+    full_weeks = num_days // 7
+    remaining_days = num_days % 7
+    
+    total_hours = (full_weeks * weekly_days + min(remaining_days, weekly_days)) * daily_hours
+    return total_hours
