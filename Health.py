8fd07ae617714786e38ce3b4ecf3f087d826dbef
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-09-02 15:19:53 +0300

    training and saving the model

--- Health.py
+++ Health.py
@@ -50,3 +50,7 @@ X = df_encoded.drop(['Exercise Recommendation Plan', 'Exercise Plan Description'
 y = df_encoded[['Exercise Recommendation Plan']]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = XGBRegressor()
+model.fit(X_train, y_train)
+joblib.dump(model, 'model.pkl')
