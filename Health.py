41fb926119c158a8aae75bb663a306c70039c438
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-09-02 15:20:25 +0300

    loading the model

--- Health.py
+++ Health.py
@@ -54,3 +54,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = XGBRegressor()
 model.fit(X_train, y_train)
 joblib.dump(model, 'model.pkl')
+
+model = joblib.load('model.pkl')
