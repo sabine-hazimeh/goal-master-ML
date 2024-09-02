b9170db426684f9006af82e25ec510ab5616cbcc
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-09-02 15:08:58 +0300

    removing outliers using IQR method

--- Health.py
+++ Health.py
@@ -17,3 +17,12 @@ CORS(app)
 df = pd.read_csv('C:/Users/Admin/Downloads/final_dataset.csv')
 
 df = df.dropna()
+
+def remove_outliers(df, column):
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    df = df[(df[column] >= lower_bound) & (df[column] <= upper_bound)]
+    return df
